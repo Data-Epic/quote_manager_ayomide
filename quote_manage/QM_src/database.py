61012a9ac227cbb4efd893ba1086b06b34f4a2fd
Ayomide James <75287859+dammy230@users.noreply.github.com>
2024-09-11 01:03:35 +0100

    database.py

--- quote_manage/QM_src/database.py
+++ quote_manage/QM_src/database.py
@@ -3,7 +3,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
-from .models import Base
+from QM_src.models import Base
 from contextlib import contextmanager
 
 
